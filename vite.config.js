f65e6d7f631f418469d0f3c1893f6cd2e07eb2a5
Tarun Kumar <bhatttarun959@gmail.com>
2025-12-14 14:26:32 +0530

    delpoyment error

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/tarunkumar1504/gitgrade-repo-project',
+  base: '/https://github.com/tarunkumar1504/gitgrade-repo-project/',
   plugins: [react(),
     tailwindcss(),
   ],
