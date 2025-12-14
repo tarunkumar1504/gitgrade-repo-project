da66455bd32aa78b7488658f2f508f3835884802
Tarun Kumar <bhatttarun959@gmail.com>
2025-12-14 15:35:10 +0530

    fix deploy

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -5,7 +5,7 @@ const ScoreCard = ({ score, level }) => {
     <motion.div
       initial={{ scale: 0.8, opacity: 0 }}
       animate={{ scale: 1, opacity: 1 }}
-      className="bg-gradient-to-r from-indigo-600 to-purple-600 p-6 rounded-xl text-center"
+      className=" from-indigo-600 to-purple-600 p-6 rounded-xl text-center"
     >
       <p className="text-4xl font-bold">{score} / 100</p>
       <p className="text-lg mt-1">{level}</p>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/gitgrade-repo-project/',
+  base: './',
   plugins: [react(),
     tailwindcss(),
   ],
