d53e29273035294250c5cb1b8d1ca7a5c7754141
Tarun Kumar <bhatttarun959@gmail.com>
2025-12-14 15:38:47 +0530

    fix error

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/gitgrade-repo-project/',
   plugins: [react(),
     tailwindcss(),
   ],
